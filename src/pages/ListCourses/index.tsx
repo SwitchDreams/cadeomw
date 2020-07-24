73ddc9679730e529aee789563de6676d66b4efb0
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-07-24 00:31:46 -0300

    Index Lista de Cursos Inicial

--- src/pages/ListCourses/index.tsx
+++ src/pages/ListCourses/index.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-
-import { Container } from './styles';
+import { Container, Courses } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 
 /*
   Página de listagem de cursos - Waliff
 */
 
+import Header from '../../components/Header';
+
 const ListCourses: React.FC = () => {
-  return <Container>Lista de Cursos</Container>;
+  return (
+    <>
+      <Header />
+      <Courses>
+        <a href="teste">
+          <div>
+            <strong> Nome do Curso</strong>
+            <p>Código</p>
+            <p>Número de Períodos</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <div>
+            <strong> Nome do Curso</strong>
+            <p>Código</p>
+            <p>Número de Períodos</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <div>
+            <strong> Nome do Curso</strong>
+            <p>Código</p>
+            <p>Número de Períodos</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        
+      </Courses>
+    </>
+  );
 };
 
 export default ListCourses;
--- src/pages/ListCourses/styles.ts
+++ src/pages/ListCourses/styles.ts
@@ -1,3 +1,49 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Courses = styled.div`
+    margin-top: 60px;
+    
+    
+    a {
+
+        max-width: 700px;
+        margin: 0 auto;
+        padding: 40px 20px;
+
+        background: #f2f2f2;
+        border-radius: 5px;
+        width: 100%;
+        padding: 24px;
+        display: block;
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        transition: transform 0.2s;
+
+        & + a {
+            margin-top: 16px;
+        }
+
+        &:hover {
+         transform: translateX(10px);   
+        }
+    
+        strong {
+            font-size: 20px;
+            color: #3D3D4D;
+        }
+
+        p {
+            font-size: 18px;
+            color: #A8A8B3;
+            margin-top: 4px;
+        }
+
+        svg {
+            margin-left: auto;
+            color: #cbcbd6;
+        }
+    }
+`;
