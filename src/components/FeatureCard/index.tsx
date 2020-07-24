ada216173bb31b4f22cb64ea8464901ecce10bbf
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-07-24 15:07:58 -0300

    :sparkles: add FeatureCard component

---
+++ src/components/FeatureCard/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const FeatureCard: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="/img/featured_image_1.png" alt="" />
+        </div>
+        <div>
+          <h3>Fully functional</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+            culpa expedita dignissimos.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeatureCard;
