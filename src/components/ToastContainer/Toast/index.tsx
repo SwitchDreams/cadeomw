d2718bbc30cb552f8028e549e19ea22c4e28be0a
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-07-29 15:48:22 -0300

    Transition Toast

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -7,6 +7,7 @@ import { Container } from './styles';
 
 interface ToastProps {
     message: ToastMessage;
+    style: object;
 }
 
 const icons = {
@@ -15,7 +16,7 @@ const icons = {
     success:<FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({message}) => {
+const Toast: React.FC<ToastProps> = ({message, style}) => {
     const { removeToast } = useToast();
 
     useEffect(() => {
@@ -25,7 +26,7 @@ const Toast: React.FC<ToastProps> = ({message}) => {
         return () => clearTimeout(timer);
     }, [removeToast, message.id])
     return(
-        <Container type={message.type}>
+        <Container type={message.type} style={style}>
             {icons[message.type || 'info']}
             <div>
                 <strong> {message.title} </strong>
--- src/components/ToastContainer/Toast/styles.ts
+++ src/components/ToastContainer/Toast/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { animated } from 'react-spring';
 
 interface ContainerProps {
     type?: 'success' | 'error' | 'info';
@@ -19,7 +20,7 @@ const ToastTypeVariations = {
     `
 }
 
-export const Container = styled.div<ContainerProps>`
+export const Container = styled(animated.div)<ContainerProps>`
     width: 360px;
 
     position: relative;
