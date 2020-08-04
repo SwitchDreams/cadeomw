0d5937bdf17abc769a8855ff02e957bfce33837b
brunaazambuja <bubu1azambuja@gmail.com>
2020-08-04 18:53:48 -0300

    Fixing floating footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import GlobalStyles from './styles/global';
+import GlobalStyles, { AllContainer } from './styles/global';
 
 import Routes from './routes';
 import Footer from './components/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ToastProvider } from './hooks/toasts'
+import { ToastProvider } from './hooks/toasts';
 
 const App: React.FC = () => {
   return (
     <>
-      <ToastProvider>
-        <BrowserRouter>
-          <Routes />
-          <GlobalStyles />
-        </BrowserRouter>
-      </ToastProvider>
+      <AllContainer>
+        <ToastProvider>
+          <BrowserRouter>
+            <Routes />
+            <GlobalStyles />
+          </BrowserRouter>
+        </ToastProvider>
+      </AllContainer>
       <Footer />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   * {
@@ -26,4 +26,8 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+`;
+
+export const AllContainer = styled.div`
+  min-height: 100vh;
 `;
