400691a0b0e53aa9796020dce41893b40292d3e4
lucasazuma <azumalucas@gmail.com>
2020-08-06 10:40:42 -0300

    criação do html

--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -251,10 +251,6 @@ const Course: React.FC = () => {
             </CardFluxContainer>
           </AllContainer>
         )}
-        <head>
-          <script data-ad-client="ca-pub-9432744401324317" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
-          </script>
-        </head>
       </AllContainer>
     </>
   );
