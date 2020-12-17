70697a39931f003366c133cea2b4c34449743fc0
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-12-17 16:12:53 -0300

    Refatorando Setup

--- course/scripts/db_setup.py
+++ course/scripts/db_setup.py
@@ -1,15 +1,47 @@
-from course.scripts.parse_fluxo import run as run_parse_fluxo
-from course.scripts.parse_mencao import run as run_parse_mencao
-from course.scripts.parse_equivalencias import run as run_parse_equivalencias
-from course.scripts.refactor_course_name import run as run_refactor_course_name
+from course.scripts.sigaa.parse_departments import parse_departments
+from course.scripts.sigaa.parse_oferta import run as parse_oferta
+from course.scripts.sigaa.parse_curriculo import parse_curriculum
+from course.scripts.sigaa.parse_fluxo import parse_course
+from course.scripts.sigaa.parse_equivalence import parse_equivalence
+
 from course.models.models import Course, Subject
 
+# Faz o parse das informações
+def run():
 
-def pre_process_courses():
-    """ Pré-calculando dados no banco de dados para poupar processamento futuro """
-    print("Processando dados dos cursos...")
+    # Cria os cursos e os departamentos
+    print("\n###### INICIANDO O PARSE DOS DEPARTAMENTOS ######\n")
+    parse_departments()
+    print("\n###### PARSE DOS DEPARTAMENTOS CONCLUÍDO ######\n")
+
+    # Cria as matérias e suas turmas da oferta
+    print("\n###### INICIANDO O PARSE DA OFERTA ######\n")
+    parse_oferta()
+    print("\n###### PARSE DA OFERTA CONCLUÍDO ######\n")
+
+    # Get all course IDs 
+    course_ids = [course.code for course in Course.objects.all()]
+
+    print("\n###### INICIANDO O PARSE DOS CURSOS ######\n")
+    for course_id in course_ids:
+        print(f"\n###### INICIANDO O PARSE DO CURSO {course_id} ######\n")
+        parse_course(course_id)
+        print("\n###### PARSE DO FLUXO CONCLUÍDO ######\n")
+
+        parse_curriculum(course_id)
+        print("\n###### PARSE DO CURRÍCULO CONCLUÍDO ######\n")
+
+    print("\n###### ADICIONANDO EQUIVALÊNCIAS e PRÉ-REQUISITOS NA DISCIPLINA ######\n")
+    subjects_ids = [subject.code for subject in Subject.objects.all()]
+    for subjects_id in subjects_ids:
+        print("\n###### PARSE EQUIVALÊNCIAS e PRÉ-REQUISITOS ######\n")
+        parse_equivalence(subjects_id)
+
+
+    print("\n###### PRE PROCESSANDO OS DADOS DOS CURSOS ######\n")
     for course in Course.objects.all():
         course.flow = course.get_flow()
+        course.curriculum = course.get_curriculum()
         course.num_semester = course.get_num_semester()
         course.hardest_subject = course.get_hardest_subject()
         course.easiest_subject = course.get_easiest_subject()
@@ -17,22 +49,10 @@ def pre_process_courses():
             course.flow_graph = course.get_flow_graph()
         course.save()
 
-
-def pre_process_subjects():
-    """ Pré-processando dados das matérias """
+    print("\n###### PRE PROCESSANDO OS DADOS DAS MATÉRIAS ######\n")
     for subject in Subject.objects.all():
         subject.pass_percent = subject.get_pass_percent()
         subject.prerequisites = subject.get_prerequisites()
         subject.equivalences = subject.get_equivalences()
         subject.grade_infos = subject.get_grade_infos()
         subject.save()
-
-
-def run():
-    # Faz o parse das informações
-    run_parse_fluxo()
-    run_parse_mencao()
-    run_parse_equivalencias()
-    pre_process_subjects()
-    pre_process_courses()
-    run_refactor_course_name()
