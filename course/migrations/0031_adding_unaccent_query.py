8d4e4580c8e7a8251916a0305eec7c62bcc78e48
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2020-12-27 12:44:05 -0300

    mudando ordem da criação

--- course/migrations/0031_adding_unaccent_query.py
+++ course/migrations/0031_adding_unaccent_query.py
@@ -4,7 +4,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('course', '0028_subject_offer'),
+        ('course', '0030_auto_20201224_2043'),
     ]
 
     operations = [
