9066cae05021b4420f8f0230f39569b970e7e09c
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-12-28 06:25:31 -0300

    Proibindo turmas com nomes iguais

--- course/models/models.py
+++ course/models/models.py
@@ -317,7 +317,7 @@ class Option(models.Model):
 
 class Offer(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='offers')
-    name = models.CharField(max_length=100)
+    name = models.CharField(unique=True, max_length=100)
     semester = models.CharField(max_length=7)
     schedule = models.CharField(max_length=100)
     students_qtd = models.CharField(max_length=3)
