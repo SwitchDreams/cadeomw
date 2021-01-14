875401fba962dd4fe543b36e24a8651f6169e388
Waliff Cordeiro Bandeira <43971264+waliffcordeiro@users.noreply.github.com>
2021-01-14 08:31:11 -0300

    Alterando PicPay

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -53,7 +53,7 @@ const ContactUs: React.FC = () => {
     // Creates a dummy element, so we can copy it's value to clipboard
     const dummy = document.createElement('textarea');
     document.body.appendChild(dummy);
-    dummy.value = 'https://picpay.me/cadeomw';
+    dummy.value = 'https://picpay.me/waliffcordeiro';
     dummy.select();
     document.execCommand('copy');
     document.body.removeChild(dummy);
