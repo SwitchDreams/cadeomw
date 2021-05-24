e09298db756a62167700ad4e1c2aed03676b76c9
João Pedro Assis <joaopedroassisdossantos@gmail.com>
2021-05-24 00:29:49 -0300

    removes old styling

--- src/pages/ListSubjects/styles.ts
+++ src/pages/ListSubjects/styles.ts
@@ -3,7 +3,7 @@ import { shade } from 'polished';
 import { ThemeType } from '../../App';
 
 interface CoursesProps {
-  window: boolean;
+  window?: boolean;
   theme: ThemeType;
 }
 
@@ -107,65 +107,6 @@ export const Form = styled.div<CoursesProps>`
 `;
 
 export const Subjects = styled.div<CoursesProps>`
-  a {
-    max-width: 700px;
-    margin: 0 auto;
-    padding: 40px 20px;
-
-    background: ${({ theme }) => theme.colors.list_cards};
-    border-radius: 5px;
-    width: 90%;
-    padding: 24px;
-    display: block;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    transition: transform 0.2s;
-
-    p {
-      margin-bottom: 3px;
-    }
-
-    ${props =>
-      props.window &&
-      css`
-        height: 90px;
-      `}
-
-    & + a {
-      margin-top: 16px;
-    }
-
-    &:hover {
-      transform: translateX(10px);
-    }
-
-    strong {
-      font-size: 20px;
-      color: ${({ theme }) => theme.colors.text};
-      ${props =>
-        props.window &&
-        css`
-          font-size: 16px;
-        `}
-    }
-
-    p {
-      font-size: 18px;
-      color: #a8a8b3;
-      margin-top: 4px;
-      ${props =>
-        props.window &&
-        css`
-          font-size: 12px;
-        `}
-    }
-
-    svg {
-      margin-left: auto;
-      color: #cbcbd6;
-    }
-  }
   .actions {
     max-width: 700px;
     margin: 0 auto;
