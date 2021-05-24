eef213c6cbfcbe3d5de6b0bc8777f5068d254e02
Bruna <bubu1azambuja@gmail.com>
2021-05-24 13:15:56 -0300

    Changes requested.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import MUICookieConsent from 'material-ui-cookie-consent';
-
-import GlobalStyles, { AllContainer } from './styles/global';
 
 import Routes from './routes';
 import Footer from './components/Footer';
@@ -12,16 +9,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastProvider } from './hooks/toasts';
 import createServer from './services/mock';
 
+import GlobalStyles, { AllContainer } from './styles/global';
+
 createServer();
 
 const App: React.FC = () => (
   <>
     <AllContainer>
-      <MUICookieConsent
-        cookieName="Cookies"
-        message="Este site usa cookies para melhorar a experiência do usuário. Caso deseje continuar, declara estar ciente da Política de Privacidade."
-        acceptButtonLabel="Estou ciente."
-      />
       <ToastProvider>
         <BrowserRouter>
           <Routes />
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Grid } from '@material-ui/core';
+import {
+  Grid,
+  Dialog,
+  Button as MUIButton,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from '@material-ui/core';
+
 import { BsArrowLeftRight } from 'react-icons/bs';
 import { RiCalendarCheckLine } from 'react-icons/ri';
 import {
@@ -8,6 +16,8 @@ import {
   AiFillFacebook,
 } from 'react-icons/ai';
 import { Container, Col, Row, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { getFromLS, setToLS } from '../../utils/localStorage';
 import Header from '../../components/Header';
 import FeatureCard from '../../components/FeatureCard';
 
@@ -36,6 +46,7 @@ Atualizações - Bruna
 
 const Dashboard: React.FC = () => {
   const [windowCheck, setWindowCheck] = useState(false);
+  const [cookie, setCookie] = useState(getFromLS('cookie') || false);
 
   useEffect(() => {
     if (window.innerWidth <= 1000) {
@@ -53,6 +64,26 @@ const Dashboard: React.FC = () => {
 
   return (
     <>
+      <Dialog open={!cookie}>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description" component="div">
+            Este site usa cookies para melhorar a experiência do usuário. Caso
+            deseje continuar, declara estar ciente dos{' '}
+            <Link to="/policy-terms">Termos e Condições</Link>.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <MUIButton
+            onClick={() => {
+              setCookie(true);
+              setToLS('cookie', { cookie: true });
+            }}
+            color="primary"
+          >
+            Estou ciente.
+          </MUIButton>
+        </DialogActions>
+      </Dialog>
       <WavesContainer>
         <div className="curved">
           <Header transparent />
