02b2d87b1ef69a2fd0e57cd66f194847afce181c
João Pedro Assis <joaopedroassisdossantos@gmail.com>
2021-05-24 00:25:24 -0300

    adds subject body

---
+++ src/components/ListCard/subjects.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+interface SubjectProps {
+    name: string;
+    code: string | number | undefined;
+    departmentName: string | undefined;
+}
+
+const Subject: React.FC<SubjectProps> = ({ name, code, departmentName }) => {
+    return (
+        <div>
+            <strong>
+                {name.charAt(0).toUpperCase() +
+                    name.slice(1).toLowerCase()}
+            </strong>
+            <p>Código: {code}</p>
+            <p>Departamento: {departmentName}</p>
+        </div>
+    )
+}
+
+export default Subject;
