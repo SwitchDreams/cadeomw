aed9828dbf2a5fa7f669f2c7bb2fc1085eff9729
João Pedro Assis <joaopedroassisdossantos@gmail.com>
2021-06-06 16:59:06 -0300

    api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL || "https://back.cadeomw.com.br",
 });
 
 export default api;
