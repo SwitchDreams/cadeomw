ced3d411fc200fc5d453bcd946eb04356d72c8e9
Waliff Cordeiro <waliffcordeiro@gmail.com>
2022-05-10 18:04:43 -0300

    Merge parse_fluxo and parse_curriculum into parse_course
    Co-authored-by: PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
    Co-authored-by: brunaazambuja <bubu1azambuja@gmail.com>

--- course/scripts/sigaa/parse_course.py
+++ course/scripts/sigaa/parse_course.py
@@ -10,7 +10,7 @@ DEPARTMENTS_MAP = {
     "POL": "IPOL"
 }
 
-def parse_curriculum(course_sigaa_id):
+def parse_course(course_sigaa_id):
     # Try to get the course. If its not possible, exit the function
     try:
         course = Course.objects.get(code=course_sigaa_id)        
@@ -22,7 +22,11 @@ def parse_curriculum(course_sigaa_id):
 
     # Get sigaa info to make the proper request
     url = "https://sig.unb.br/sigaa/public/curso/curriculo.jsf"
-    request_data = get_request_from_course(course_sigaa_id)
+    try:
+        request_data = get_request_from_course(course_sigaa_id)
+    except:
+        print(f'O curso {course_sigaa_id} não possui currículo')
+        return
     payload = f'formCurriculosCurso=formCurriculosCurso&nivel=G&javax.faces.ViewState={request_data["javax"]}&formCurriculosCurso%3Aj_id_jsp_154341757_30=formCurriculosCurso%3Aj_id_jsp_154341757_30&id={request_data["id"]}'
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -32,6 +36,8 @@ def parse_curriculum(course_sigaa_id):
     # Get the curriculum webpage
     response = requests.request("POST", url, headers=headers, data=payload)
     html_soup = BeautifulSoup(response.text.encode('utf8'), 'html.parser')
+
+    parse_flow(html_soup, course)
     
     header_soup = html_soup.find_all('tr', limit=15)
     optional_subjects_soup = html_soup.find(id='optativas')
@@ -122,6 +128,100 @@ def parse_curriculum(course_sigaa_id):
             print(f"    Could not link subject {curr_subject['name']} with its course")
             continue
 
+def parse_flow(html_soup, course):
+    semesters = html_soup.find("div", {"class": 'yui-content'})
+    if semesters:
+        # Verificando se não está vazio
+        semesters = semesters.find_all("div")[2:]
+    for semester in semesters:
+        semester_number = semester["id"][8:]
+        # Do not get the last term Ex: Carga Horária Total: 360hrs.
+        semester_subjects = semester.find_all("tr")[:-1]
+        for subject in semester_subjects:
+            save_subject(course, subject, semester_number)
+
+
+def save_subject(course, html_subject, semester):
+    html_subject_rows = html_subject.find_all("td")
+    subject_info = html_subject_rows[0].text.split(' - ')
+    status = html_subject_rows[1].text.strip()
+    code = subject_info[0]
+
+    # Prevent that subject names with "-" are wrongly parsed
+    name_components = subject_info[1:-1]
+    name = name_components[0]
+    for component in name_components[1:0]:
+        name += ' - ' + component
+    
+    # Get credits in hours format
+    credit = int(subject_info[-1][:-1])
+
+    # Get status code
+    status_code = {
+        "Obrigatória":  "OBR",
+        "Optativa":     "OPT",
+        "Módulo Livre": "ML"
+    }
+
+    # Try to get current subject from the DB
+    # If its not there, proceed to create it
+    try:
+        subject = Subject.objects.get(code=code)
+    except Exception as error:
+        print(f"Could not find subject {name}: {error}. Trying to create it")
+        dept_initials = code[:3]
+    
+        # If the subject can't be found, get its department from its code
+        # Try to find the department
+        try:
+            department = Department.objects.get(initials=dept_initials)
+        except Exception as error:
+
+            # If the department cannot be found, check if its initials are mapped
+            # If the initials are not mapped or the mapped one fails, proceed to create subject without department
+            if dept_initials in DEPARTMENTS_MAP:
+                new_deps_initials = DEPARTMENTS_MAP[dept_initials]
+
+                try:
+                    department = Department.objects.get(initials=new_deps_initials)
+                except Exception as error:
+                    print(f"Could not find correct department: {error}. Creating subject {name} without department")
+                    
+            else:
+                print(f"Could not find alternative department code: {error}. Creating subject {name} without department")
+
+            # Creates subject without department
+            try:
+                subject = Subject(
+                    name=name,
+                    code=code,
+                    credit=credit
+                )
+                subject.save()
+                print(f"Subject {name} created without department")
+            except Exception as error:
+                # If all of the above fails, skip to the next subject
+                print(f"Could not create subject {name}: {error}. Skipping subject")
+                return
+
+        # Create subject if department is found
+        try:
+            subject = Subject(
+                name=name,
+                department=department,
+                code=code,
+                credit=credit
+            )
+            subject.save()
+            print(f"Subject {name} created")
+        except IntegrityError:
+            print(f"Subject {name} is already created")
+        except Exception as error:
+            print(f"Could not create subject {name}: {error}. Skipping subject")
+            return
+
+    # Append the created subject       
+    course.append_subject(semester, subject, status_code[status])
 
 def get_request_from_course(course_sigaa_id):
     # Get cookies and auth info
@@ -180,5 +280,5 @@ def get_optional_subject_info(opt_soup):
 
 def run():
     # Example data from Engenharia da computação
-    parse_curriculum(414610)
+    parse_course(414610)
 
