0c11e27376b4de2a5145e96c3155687e67886964
Waliff Cordeiro <waliffcordeiro@gmail.com>
2022-05-10 18:59:19 -0300

    Update db_setup
    Co-authored-by: brunaazambuja <bubu1azambuja@gmail.com>
    Co-authored-by: PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>

--- course/scripts/db_setup.py
+++ course/scripts/db_setup.py
@@ -1,8 +1,7 @@
 from course.scripts.sigaa.parse_departments import parse_departments
 from course.scripts.sigaa.parse_oferta import run as parse_oferta
 from course.scripts.sigaa.parse_subjects import run as parse_disciplinas
-from course.scripts.sigaa.parse_curriculo import parse_curriculum
-from course.scripts.sigaa.parse_fluxo import parse_course
+from course.scripts.sigaa.parse_course import parse_course
 from course.scripts.sigaa.parse_equivalence import parse_equivalence
 from course.scripts.refactor_course_name import run as refactor_course
 from course.scripts.sigaa.parse_unidade import run as parse_unidades
@@ -13,20 +12,20 @@ from course.models.models import Course, Subject
 # Faz o parse das informações
 def run():
     # Cria os cursos e os departamentos
-    # print("\n###### INICIANDO O PARSE DOS DEPARTAMENTOS ######\n")
-    # parse_departments()
-    # parse_unidades()
-    # print("\n###### PARSE DOS DEPARTAMENTOS CONCLUÍDO ######\n")
+    print("\n###### INICIANDO O PARSE DOS DEPARTAMENTOS ######\n")
+    parse_departments()
+    parse_unidades()
+    print("\n###### PARSE DOS DEPARTAMENTOS CONCLUÍDO ######\n")
 
-    # print("\n###### INICIANDO PARSE DAS DISCIPLINAS ######\n")
-    # # Cria todas as disciplinas com base nos departamentos
-    # parse_disciplinas()
-    # print("\n######  PARSE DAS DISCIPLINAS CONCLUÍDO ######\n")
+    # Cria todas as disciplinas com base nos departamentos
+    print("\n###### INICIANDO PARSE DAS DISCIPLINAS ######\n")
+    parse_disciplinas()
+    print("\n######  PARSE DAS DISCIPLINAS CONCLUÍDO ######\n")
 
     # Cria as matérias e suas turmas da oferta
-    # print("\n###### INICIANDO O PARSE DA OFERTA ######\n")
-    # parse_oferta()
-    # print("\n###### PARSE DA OFERTA CONCLUÍDO ######\n")
+    print("\n###### INICIANDO O PARSE DA OFERTA ######\n")
+    parse_oferta()
+    print("\n###### PARSE DA OFERTA CONCLUÍDO ######\n")
 
     # Get all course IDs 
     course_ids = [course.code for course in Course.objects.all()]
@@ -34,11 +33,9 @@ def run():
     print("\n###### INICIANDO O PARSE DOS CURSOS ######\n")
     for course_id in course_ids:
         print(f"\n###### INICIANDO O PARSE DO CURSO {course_id} ######\n")
-        parse_course(course_id)
-        print("\n###### PARSE DO FLUXO CONCLUÍDO ######\n")
 
-        parse_curriculum(course_id)
-        print("\n###### PARSE DO CURRÍCULO CONCLUÍDO ######\n")
+        parse_course(course_id)
+        print("\n###### PARSE DO CURSO CONCLUÍDO ######\n")
 
     refactor_course()
     print("\n###### PARSE DOS CURSOS CONCLUÍDO ######\n")
